d1fddbb1a0b59721b59f8cd3c892edb1b6d43a40
chenxu10 <shenxu2011@gmail.com>
2023-02-03 05:36:42 -0500

    expect exception in pytest and create customized inner class

--- fundamentals/stack.py
+++ fundamentals/stack.py
@@ -1,4 +1,11 @@
 class Stack():
+    class UnderflowException(Exception):
+        def __init__(self, message):
+            self.message = message
+        
+        def __str__(self):
+            return self.message
+    
     def __init__(self):
         self.items = []
         self.isEmpty = True
@@ -9,3 +16,13 @@ class Stack():
     def push(self, item):
         self.items.append(item)
         self.isEmpty = False
+
+    def pop(self):
+        if self.is_empty():
+            raise Stack.UnderflowException('Stack is empty')
+        else:
+            self.items.pop()
+            if len(self.items) == 0:
+                self.isEmpty = True
+
+    
--- fundamentals/test_stack.py
+++ fundamentals/test_stack.py
@@ -1,13 +1,21 @@
 from stack import Stack 
+import pytest
 
 class TestStack():
-    def test_create_stack(self):
+    @pytest.fixture
+    def stack(self):
         stack = Stack()
+        return stack
+        
+    def test_create_stack(self, stack):
         assert stack.is_empty() == True
 
-    def test_push(self):
-        stack = Stack()
+    def test_afteronepush_isstillempty(self, stack):
         stack.push(1)
         assert stack.is_empty() == False
+
+    def test_willthrowunderflow_whenemptystackispopped(self, stack):
+        with pytest.raises(stack.UnderflowException):
+            stack.pop()
 
     
