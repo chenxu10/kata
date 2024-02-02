b4b0e0cc5e9d947d6e680f0b69e1e03427615d73
chenxu10 <shenxu2011@gmail.com>
2024-02-02 07:56:05 -0500

    working

--- fundamentals/test_rope_practice.py
+++ fundamentals/test_rope_practice.py
@@ -1,43 +1,37 @@
-# TCR
-
+# TCR Rope
 
 # insert
 # delete
+# concatentae
 # substring
-# concatenate
-# high level princples
-# feelings
 
-# API
-def to_rope(s):
-    return String(s)
+# last rountine call
+# trivial helper function
+# test passes TCR
+
 
 
 class Rope:
-    def substring(self, start, length):
-        return Substring(self, start, length)
-
-class String(Rope):
+    
     def __init__(self,s):
-        self.s = s
+        self.s = s 
 
     def __str__(self):
         return self.s
-    
-class Substring(Rope):
-    def __init__(self, rope, start, length):
-        self.rope = rope
-        self.start = start
-        self.length = length
 
-    def __str__(self) -> str:
-        return str(self.rope)[self.start:self.start + self.length]
-    
+
+def to_string(s):
+    return Rope(s)
 
 
 if __name__ == '__main__':
-    assert str(to_rope("abc")) == "abc"
-    assert str(to_rope("abcde").substring(1,3)) == "bcd"
-    assert str(to_rope("abcde").substring(1,3).substring(1,1)) == "c"
+   print(str(Rope("abc")))
+
+
+
+
+
+
+
 
 
