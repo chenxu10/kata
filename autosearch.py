ad10cdb4a16ad439dc6ecf99b03c82933252992a
chenxu10 <shenxu2011@gmail.com>
2024-02-26 19:55:49 -0500

    working

---
+++ autosearch.py
@@ -0,0 +1,14 @@
+import webbrowser 
+import sys
+
+def search_terms():
+    if len(sys.argv) > 1:
+        query = ' '.join(sys.argv[1:])
+    else:
+        query = input("Enter search query: ")
+
+    #webbrowser.get('chrome').open_new_tab('https://www.google.com/search?q=' + query)
+
+if __name__ == '__main__':
+    ###
+    search_terms()
