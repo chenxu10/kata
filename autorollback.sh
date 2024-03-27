40a04697ff001c302d819151c7f128164e99c308
chenxu10 <shenxu2011@gmail.com>
2024-03-27 13:47:01 -0400

    add autorollback.sh

---
+++ autorollback.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do
+    current_commit=$(git rev-parse HEAD)
+    sleep 30
+    new_commit=$(git rev-parse HEAD)
+
+    if ["$current_commit" = "$new_commit"]; then
+        git reset --hard HEAD
+        echo "now reverted to previous commit"
+    else
+        echo "new commit detected, no action taken"
+    fi
+done
