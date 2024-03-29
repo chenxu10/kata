e015553e4863a5ca0c992f7bbde556d6478f0c8f
chenxu10 <shenxu2011@gmail.com>
2024-03-29 08:01:01 -0400

    workinggood

--- fundamentals/test_sumpi.ml
+++ fundamentals/test_sumpi.ml
@@ -1,2 +1,8 @@
 let term a = 
-    1.0 /. (a *. (a +. 2.))
+    1.0 /. (a *. (a +. 2.0))
+
+let () = 
+    let a = 3.0 in
+    let result = term a in
+    print_endline (string_of_float result)
+(**)
