e21c9d70a7ffe3774db1564dbd8e903e3926ff19
chenxu10 <shenxu2011@gmail.com>
2024-04-04 17:29:56 -0400

    working

--- playground/test_fancy_number.py
+++ playground/test_fancy_number.py
@@ -25,3 +25,4 @@ if __name__ == '__main__':
     assert count_fancy_numbers(0) == 1
     assert count_fancy_numbers(1) == 2
     assert count_fancy_numbers(4) == 3
+    assert count_fancy_numbers(5) == 4
