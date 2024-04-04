cc4a77a4532e0c3dc095b6be57237d9ed6f051df
chenxu10 <shenxu2011@gmail.com>
2024-04-04 17:29:13 -0400

    working

--- playground/test_fancy_number.py
+++ playground/test_fancy_number.py
@@ -6,12 +6,21 @@ def count_fancy_numbers(n):
     for i in range(n + 1):
         div, mod = divmod(i, 4)
         
-        if mod == 1 or mod == 0:
-            if div != mod:
-                count += 1
+        if div == 1 and mod == 1:
+            count += 1
+
+        if div == 1 and mod == 0:
+            count += 1
+
+        if div == 0 and mod == 1:
+            count += 1
+
+        if div == 0 and mod == 0:
+            count += 1
     
     return count
 
 
 if __name__ == '__main__':
     assert count_fancy_numbers(0) == 1
+    assert count_fancy_numbers(1) == 2
