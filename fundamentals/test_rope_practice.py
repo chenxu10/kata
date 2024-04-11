1d431bfba57769e9a386d1414c434361900a7de0
chenxu10 <shenxu2011@gmail.com>
2024-04-11 07:27:21 -0400

    working

--- fundamentals/test_rope_practice.py
+++ fundamentals/test_rope_practice.py
@@ -9,14 +9,18 @@
 
 
 class Rope:
-    def __init__(self, string):
-        self.string = string
+    def __init__(self, s):
+        self.s = s
     
     def __str__(self):
-        return self.string
+        return self.s
+
+    def substring(self, start, length):
+        self.s = self.s[start: start + length]
 
 def to_rope(x):
     return Rope(x)
 
 if __name__ == '__main__':
     assert str(to_rope("abc")) == "abc"
+    print(str(to_rope("abcde").substring(1,3)))
