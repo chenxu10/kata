3e066c675afd9c6727e7aa5e2be58cf02b242e05
chenxu10 <shenxu2011@gmail.com>
2024-04-11 14:23:00 -0400

    working

--- playground/test_build_apes_graph.py
+++ playground/test_build_apes_graph.py
@@ -5,7 +5,7 @@ Implementation of p154 of chimpanzee politics
 def build_graph():
     graph = {'Luit':set(['Nikkie','Yeron'])}
     return graph
-
+ 
 if __name__ == '__main__':
     graph = build_graph()
     assert graph['Luit'] == set(['Nikkie','Yeron'])
