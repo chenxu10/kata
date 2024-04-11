9fd16e99814685710dd81c5c7e0d714f4e6cb3da
chenxu10 <shenxu2011@gmail.com>
2024-04-11 07:46:37 -0400

    working

--- fundamentals/test_rope_practice.py
+++ fundamentals/test_rope_practice.py
@@ -24,4 +24,4 @@ def to_rope(x):
 if __name__ == '__main__':
     assert str(to_rope("abc")) == "abc"
     assert str(to_rope("abcde").substring(1,3).substring(1,1)) == "c"
-    print(str(to_rope("abcdefg").substring(1,3).substring(1,2).substring(1,1)))
+    print(str(to_rope("abcdefg").substring(1,3).substring(1,2)))
