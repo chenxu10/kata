f6a44fa9324305c5d2b47a8085143b32bdd4da0a
chenxu10 <shenxu2011@gmail.com>
2024-04-11 14:20:59 -0400

    playgtound

---
+++ playground/test_build_apes_graph.py
@@ -0,0 +1,8 @@
+def build_graph():
+    graph = {'Luit':set(['Nikkie','Yeron'])}
+    return graph
+
+
+if __name__ == '__main__':
+    graph = build_graph()
+    assert graph['Luit'] == set(['Nikkie','Yeron'])
