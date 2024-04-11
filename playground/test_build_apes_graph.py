f686e00a77aee3bb9aef65d73f8f56268ffe842d
chenxu10 <shenxu2011@gmail.com>
2024-04-11 14:37:13 -0400

    working

--- playground/test_build_apes_graph.py
+++ playground/test_build_apes_graph.py
@@ -4,12 +4,16 @@ Implementation of p154 of chimpanzee politics
 from collections import defaultdict, namedtuple
 
 def build_graph():
-    match_chimp = namedtuple('connectchimp',('Luit','Tepel'))
+    match_chimp = namedtuple('connectchimp',('chimp1','chimp2'))
     match_chimps = []
+    match_chimp.chimp1 = 'Luit'
+    match_chimp.chimp2 = 'Yeron'
     match_chimps.append(match_chimp)
     graph = defaultdict(set)
+    
     for chimps in match_chimps:
-        print(chimps)
+        graph[chimps.chimp1].add(chimps.chimp2)
+        print(graph)       
     return graph
  
 if __name__ == '__main__':
