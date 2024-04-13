6372650956814d725812bbf4783f0014ae466cbe
chenxu10 <shenxu2011@gmail.com>
2024-04-13 13:54:14 -0400

    working

--- playground/worsttrader.py
+++ playground/worsttrader.py
@@ -3,5 +3,7 @@ Optiver worst trader
 OOP exercises
 
 https://leetcode.com/discuss/interview-question/3812812/Optiver-or-OA-or-Worst-Trade-Reporter
+
+https://docs.anthropic.com/claude/reference/messages_post
 """
 
