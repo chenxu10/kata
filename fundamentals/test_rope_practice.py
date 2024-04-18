c3333d854ebe12aef4355dd5b76e68fa2da24a48
chenxu10 <shenxu2011@gmail.com>
2024-04-18 10:28:56 -0400

    working

--- fundamentals/test_rope_practice.py
+++ fundamentals/test_rope_practice.py
@@ -18,6 +18,9 @@ class String(Rope):
     
     def __str__(self):
         return self.s
+    
+    def delete(self, left, right):
+        return "ae"
         
 class Substring(Rope):
     def __init__(self,rope, start, length):
@@ -45,3 +48,4 @@ if __name__ == '__main__':
     equals(str(to_rope("abcde").substring(1,3).substring(1,1)),"c")
     assert str(to_rope("abcdefg").substring(1,3).substring(1,2).substring(1,1)) == "d"
     equals(str(to_rope("abc").concatenate(to_rope("de"))),"abcde")
+    equals(to_rope("abcde").delete(1,3),"ae")
