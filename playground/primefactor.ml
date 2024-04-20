f3102e63357734bbf17cc0af3dd7cd27cfc78732
chenxu10 <shenxu2011@gmail.com>
2024-04-20 12:48:29 -0400

    workinggood

--- playground/primefactor.ml
+++ playground/primefactor.ml
@@ -5,7 +5,7 @@ let test_primefactor =
     assert (primefactor 1 = []);;
     assert (primefactor 2 = []);;
 
-(* can you define a sqaure function *)
+(* can you define a sqaure functions sd *)
 let square x = x * x;;
 let sol = square 2;;
 print_int sol;;
