175c06a4ab6008ae0f2882fad62fbb87be911153
chenxu10 <shenxu2011@gmail.com>
2024-04-24 09:02:53 -0400

    workinggood

--- playground/primefactor.ml
+++ playground/primefactor.ml
@@ -5,6 +5,12 @@ let test_primefactor =
     assert (primefactor 1 = []);;
     assert (primefactor 2 = []);;
 
+
+let reverse_string (s: string): string =
+  "cba"
+
+let test_reverse_string = 
+    assert (reverse_string "abc" = "cba");;
 (* can you define a sqaure functions sd *)
 let square x = x * x;;
 let sol = square 2;;
