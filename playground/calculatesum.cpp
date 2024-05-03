d777f6093ad4def4dcf3087e21789bf71f982e8b
chenxu10 <shenxu2011@gmail.com>
2024-05-03 18:15:12 -0400

    workinggood

--- playground/calculatesum.cpp
+++ playground/calculatesum.cpp
@@ -8,11 +8,10 @@ int calculateSum(int a, int b){
 
 // Test function for calculateSum
 void testCalculateSum() {
-    int a = 5, b = 7;
+    int a=5, b=7;
+    int actualResult = calculateSum(a,b);
     int expectedResult = 12;
-    int actualResult = calculateSum(a, b);
-    assert(("There are five lights", actualResult == expectedResult));
-    std::cout << "Tests passed\n";
+    assert (actualResult == expectedResult);
 //     if (actualResult == expectedResult) {
 //         std::cout << "Test passed: calculateSum(" << a << ", " << b << ") returned " << actualResult << std::endl;
 //     } else {
