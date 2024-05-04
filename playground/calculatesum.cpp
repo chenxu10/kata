0909d67e787969269f152f5bb00aa63aa20baf2e
chenxu10 <shenxu2011@gmail.com>
2024-05-04 09:24:19 -0400

    workinggood

--- playground/calculatesum.cpp
+++ playground/calculatesum.cpp
@@ -2,24 +2,27 @@
 #include <cassert>
 
 // Function to calculate the sum of two numbers
-int calculateSum(int a, int b){
+
+int calculate_sum(int a, int b){
     return a + b;
 }
 
-// Test function for calculateSum
-void testCalculateSum() {
-    int a = 5, b = 8;
-    int actualResult = calculateSum(a,b);
-    int expectedResult = 13;
+void test_calculateSum(){
+    int a = 5, b = 9;
+    int actualResult = calculate_sum(a,b);
+    int expectedResult = 14;
     assert (actualResult == expectedResult);
+}
+// Test function for calculateSum
+
 //     if (actualResult == expectedResult) {
 //         std::cout << "Test passed: calculateSum(" << a << ", " << b << ") returned " << actualResult << std::endl;
 //     } else {
 //         std::cout << "Test failed: calculateSum(" << a << ", " << b << ") returned " << actualResult << ", but expected " << expectedResult << std::endl;
 //     }
 // }
-}
+
 int main(){
-    testCalculateSum();
+    test_calculateSum();
     return 0;
 }
