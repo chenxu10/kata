aadfe67e766dd45b21d55bb54ba427c72300c2d8
chenxu10 <shenxu2011@gmail.com>
2024-07-16 09:11:51 -0400

    working

--- fundamentals/test_primefactor.py
+++ fundamentals/test_primefactor.py
@@ -1,39 +1,12 @@
-import pytest
 
-def primefactor(n:int):
-    factors = []
-    divisor = 2
-    while n > 1:
-        while n % divisor == 0:
-            factors.append(divisor)
-            n //= divisor
-        divisor += 1
-    return factors
+def prime_factor(x):
+    return []
 
-class TestPrimeFacor():
-    assert primefactor(1) == []
-    assert primefactor(2) == [2]
-    assert primefactor(3) == [3]
-    assert primefactor(4) == [2,2]
-    assert primefactor(5) == [5]
-    assert primefactor(6) == [2,3]
-    assert primefactor(8) == [2,2,2]
-    assert primefactor(9) == [3,3]
+def test_prime_factor():
+    assert prime_factor(1) == []
 
-def primefactor(n:int):
-    ans = []
-    if n == 1:
-        return ans
-    if n == 2:
-        ans.append(2)
-        return ans
-    else:
-        ans.append(n // 2)
-        primefactor(n // 2)  
+def main():
+    test_prime_factor()
 
-class TestPrimeFacor:
-    def test_primefactor(self):
-        assert primefactor(1) == []
-        assert primefactor(2) == [2]
-        #assert primefactor(3) == [3]
-        #assert primefactor(4) == [2,2]
+if __name__ == '__main__':
+    main()
--- patterns/slidingwindow/pattern.md
+++
@@ -1,4 +0,0 @@
-# Sliding Window
-+ Good at finding substring pattern matching problem
-+ Optimize to one pass
-+ Can be replaced by dynamic programming
