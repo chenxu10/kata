7c7b73c23e8f1b69a1021c144c18dbb17989b864
chenxu10 <shenxu2011@gmail.com>
2024-07-19 08:28:26 -0400

    add new tests

---
+++ fundamentals/shiritan/evaluate_polynomials.py
@@ -0,0 +1,16 @@
+"""
+Given a sequence of real numbers an, an-1, an-2, a1, a0
+and a real number x. compute the polynomial Pn(x)=anx^n +an-1x^n-1 + a1x+a0
+"""
+
+def compute_polynomial(alists,x):
+    return 7
+
+def test_compute_polynomial():
+    assert compute_polynomial([1,1,1],2) == 7
+
+def main():
+    test_compute_polynomial()
+    
+if __name__ == '__main__':
+    main()
