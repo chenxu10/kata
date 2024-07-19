9e8a19f425fc3768435552bac865e7c7d8350633
chenxu10 <shenxu2011@gmail.com>
2024-07-19 08:40:51 -0400

    working

--- fundamentals/shiritan/evaluate_polynomials.py
+++ fundamentals/shiritan/evaluate_polynomials.py
@@ -4,10 +4,14 @@ and a real number x. compute the polynomial Pn(x)=anx^n +an-1x^n-1 + a1x+a0
 """
 
 def compute_polynomial(alists,x):
-    if len(alists) == 1:
-        return alists[0]
-    else:
-        return 7
+    # if len(alists) == 1:
+    #     return alists[0]
+    
+    p = alists[-1]
+    for i in range(1,len(alists)):
+        p = x * p + alists[-1-i]
+
+    return p
 
 def test_compute_polynomial():
     assert compute_polynomial([1],2) == 1
