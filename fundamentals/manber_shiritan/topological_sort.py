292d0fc0e26fc8b7d1e2566b22e56a425abca4d0
chenxu10 <shenxu2011@gmail.com>
2024-07-24 10:59:29 -0400

    working

--- fundamentals/manber_shiritan/topological_sort.py
+++ fundamentals/manber_shiritan/topological_sort.py
@@ -16,7 +16,7 @@ def topologial_sort(G):
         stack.appendleft(node)
 
     for vertex in G:
-        if vertex not in visted:
+        if not visted[vertex]:
             dfs(vertex)
 
     return list(stack)
