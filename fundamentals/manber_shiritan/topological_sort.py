44b7e77a34926fe6fa72627b4c9125dfa3e88d16
chenxu10 <shenxu2011@gmail.com>
2024-07-24 10:53:04 -0400

    working

--- fundamentals/manber_shiritan/topological_sort.py
+++ fundamentals/manber_shiritan/topological_sort.py
@@ -5,5 +5,21 @@ def topologial_sort(G):
     return ['B','A','D','C','E','H','F','G']
 
 def test_topologial_sort():
+    G = {
+        'A':['C'],
+        'B':['C','D'],
+        'C':['E'],
+        'D':['F'],
+        'E':['H','F'],
+        'F':['G'],
+        'G':[],
+        'H':[]
+    }
     sorted_stack = topologial_sort(G)
     assert sorted_stack == ['B','A','D','C','E','H','F','G']
+
+
+def main():
+    test_topologial_sort()
+if __name__ == '__main__':
+    main()
