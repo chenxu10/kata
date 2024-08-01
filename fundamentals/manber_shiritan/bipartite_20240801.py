91ad17ef6ab47c42d166e2da1f108e2570691a84
chenxu10 <shenxu2011@gmail.com>
2024-08-01 14:29:48 -0400

    sds

---
+++ fundamentals/manber_shiritan/bipartite_20240801.py
@@ -0,0 +1,34 @@
+
+def bipartite(G):
+    visited = {node:False for node in G}
+    color = {node:-1 for node in G}
+
+    def dfs(node, c):
+        "color two different colors in an edge"
+        visited[node] = True
+        color[node] = c
+
+        for nei in G[node]:
+            if (not visited[nei]) and (not dfs(nei, 1 - c)):
+                return False
+            elif color[nei] == color[node]:
+                return False           
+        return True
+
+    for node in G:
+        if (not visited[node]) and (not dfs(node, 0)):
+            return False
+
+    return True
+
+def test_bipartite():
+    G = {'A':['B','C'],
+         'B':['A','D'],
+         'C':['A','D'],
+         'D':['B','C']}
+    assert bipartite(G) == True
+
+def main():
+    test_bipartite()
+if __name__ == '__main__':
+    main()
