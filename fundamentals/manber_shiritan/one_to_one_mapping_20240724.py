95f99b8abe9c46f23e44d9a1fbc938c7b529ec0e
chenxu10 <shenxu2011@gmail.com>
2024-08-02 17:54:35 -0400

    working

--- fundamentals/manber_shiritan/one_to_one_mapping_20240724.py
+++ fundamentals/manber_shiritan/one_to_one_mapping_20240724.py
@@ -5,46 +5,34 @@
 # we try to find the eaisiest way to remove an element without changing the conditions
 # of the problem.
 
-def max_subset(A, f):
-    visited = set()
-    max_set = set()
+'''
+Mapping:
+1 -> 3
+2 -> 1
+3 -> 1
+4 -> 5
+5 -> 5
+6 -> 4
+7 -> 6
+'''
 
-    def dfs(element):
-        """
-        This set returns the maximum element so far
-        """
-        visited.add(element)
-        next_element = f[element]
-        
-        # No two elements are mapped in the same element
-        if next_element in visited:
-            result = set()
-        else:
-            result = dfs(next_element)
+import queue
+def Algorithm_Mapping(f,n):
+    S = [i for i in range(n)]
+    c = [0 for i in range(n)]
+    for i in range(n):
+        c[f[i]] += 1
+    Queue = queue.Queue()
+    for j in range(n):
+        if c[j] == 0:
+            Queue.put(j)
+    while not Queue.empty():
+        i = Queue.get()
+        S.remove(i)
+        c[f[i]] -= 1
+        if c[f[i]] == 0: Queue.put(f[i])      
+    return S
 
-        result.add(element)
-        visited.remove(element)
-        return result
-
-    for element in A:
-        current_set = dfs(element)
-        if len(current_set) > len(max_set):
-            max_set = current_set
-
-    return max_set
-
-def main():
-    A = {1, 2, 3, 4, 5}
-    f = {1: 2, 2: 3, 3: 1, 4: 5, 5: 4}
-    result = max_subset(A, f)
-    print(result)    
-
-def main2():
-    A = {1, 2}
-    f = {1: 2, 2: 2}
-    result = max_subset(A, f)
-    print(result)    
-
-
-if __name__ == '__main__':
-    main2()
+f = [2, 0, 0, 4, 4, 3, 5]
+n = 7
+print(Algorithm_Mapping(f,n))
