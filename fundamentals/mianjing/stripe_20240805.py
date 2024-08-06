91669654cb3a26bbd76e5cb955dca27dda5aa610
chenxu10 <shenxu2011@gmail.com>
2024-08-06 15:32:01 -0400

    working

--- fundamentals/mianjing/stripe_20240805.py
+++ fundamentals/mianjing/stripe_20240805.py
@@ -12,3 +12,13 @@ def test_get_direct_rate():
     rates_string = "USD:AUD:1.4,CAD:USD:0.8,USD:JPY:110"
     converter = CurrencyConverter(rates_string)   
     print(converter.get_direct_rate("USD", "AUD"))  # 1.4
+
+
+    # Part 2: 通過一次轉換計算轉換率
+    print(converter.get_one_hop_rate("CAD", "AUD"))  # 1.12 (CAD -> USD -> AUD)
+
+    # Part 3: 使用最佳轉換率
+    print(converter.get_best_rate("USD", "AUD"))  # 1.4
+
+    # Part 4: 返回所有可計算的轉換率對
+    print(converter.get_all_possible_rates())
