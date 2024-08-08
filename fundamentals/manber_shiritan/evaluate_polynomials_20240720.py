8a15af64ff83f569a7e77c5552edb1843be8547b
chenxu10 <shenxu2011@gmail.com>
2024-08-08 14:57:51 -0400

    working

--- fundamentals/manber_shiritan/evaluate_polynomials_20240720.py
+++ fundamentals/manber_shiritan/evaluate_polynomials_20240720.py
@@ -22,8 +22,7 @@ def test_compute_polynomial():
     #assert compute_polynomial([1],1) == 1
     #assert compute_polynomial([1,1],2) == 3
     assert compute_polynomial([3,2,1],2) == 3 + 2 * 2 + 1 * 4
-    #assert compute_polynomial([3,2,1],2) == 3 * (2*2) + 2 * (2*1) + 1
-    #assert compute_polynomial([1,2,2,1],1) == 1 * 1 + 2 * 1 * 1 + 2 * 1 + 1    
+    assert compute_polynomial([1,2,2,1],1) == 1 + 2 * 1 + 2 * 1 + 1* 1
 
 def main():
     test_compute_polynomial()
