548dcacf28301dd9165e128095b6cc327f18d8ec
chenxu10 <shenxu2011@gmail.com>
2024-08-09 20:00:28 -0400

    working

--- fundamentals/manber_shiritan/evaluate_polynomials_02.py
+++ fundamentals/manber_shiritan/evaluate_polynomials_02.py
@@ -1,26 +1,23 @@
 """
-Given a sequence of real numbers an, an-1, an-2, a1, a0
-and a real number x. compute the polynomial Pn(x)=anx^n +an-1x^n-1 +a2x^2+... a1x+a0
-
-Pn-1(x) =  an-1x^n-1 +a2x^2+... a1x+a0
-Pn(x) = x*an + Pn-1(x)
-
-Pn-1(x) = anx^n-1 + an-1x^n-2 + ... +a1
-Pn(x) = x* Pn-1(x) + a0
+# Python program for
+# implementation of Horner Method 
+# for Polynomial Evaluation
+ 
+# returns value of poly[0]x(n-1)
+# + poly[1]x(n-2) + .. + poly[n-1]
 """
 
 def compute_polynomial(alists,x):
-    pn = alists[-1]
+    pn = alists[0] #an
 
     for i in range(1, len(alists)):
-        a0 = alists[-1 - i]        
-        pn = x* pn + a0
+        ai = alists[i]        
+        pn = x * pn + ai
 
     return pn
 
 def test_compute_polynomial():
-    assert compute_polynomial([3,2,1],2) == 3 + 2 * 2 + 1 * 4
-    assert compute_polynomial([1,2,2,1],1) == 1 + 2 * 1 + 2 * 1 + 1* 1
+    assert compute_polynomial([2, 0, 3, 1],2) == 2 * (2**3) + 0 + 3 * 2 + 1
 
 def main():
     test_compute_polynomial()
