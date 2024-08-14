84ae1ef6b1b9a6c27b73e13455b36a9348f2f167
chenxu10 <shenxu2011@gmail.com>
2024-08-14 15:31:36 -0400

    sds

---
+++ fundamentals/test/test_input_reduce.py
@@ -0,0 +1,6 @@
+from fundamentals.manbercreativemethod.one_to_one_mapping_04 import Algorithm_Mapping
+
+def test_algorithm_mapping():
+    f = [2, 0, 0, 4, 4, 3, 5]
+    n = 7
+    assert Algorithm_Mapping(f,n) == [0,2,4]
