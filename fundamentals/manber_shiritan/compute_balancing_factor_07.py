f5e3839a09b151b2a08b5200dd81a0ac617b8358
chenxu10 <shenxu2011@gmail.com>
2024-08-14 09:51:56 -0400

    working

--- fundamentals/manber_shiritan/compute_balancing_factor_07.py
+++ fundamentals/manber_shiritan/compute_balancing_factor_07.py
@@ -103,14 +103,12 @@ def generate_subsequence(nums):
         Reach to the leaf of the recursion tree
         """
         if start == len(nums):
-            return
+            if len(path) != 0:
+                sol.append(path[:])
 
         for i in range(start, len(nums)):
-            sol.append(path[:])
-            path.append(nums[i])
             dfs(start + 1, path + [nums[i]])
             dfs(start + 1, path)
-            path.pop()
             
 
     sol = []
