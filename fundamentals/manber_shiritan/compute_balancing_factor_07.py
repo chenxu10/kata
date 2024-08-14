6473ffe364ab360be182dc734da7b2ffc4a694d6
chenxu10 <shenxu2011@gmail.com>
2024-08-14 09:48:08 -0400

    working

--- fundamentals/manber_shiritan/compute_balancing_factor_07.py
+++ fundamentals/manber_shiritan/compute_balancing_factor_07.py
@@ -106,9 +106,10 @@ def generate_subsequence(nums):
             return
 
         for i in range(start, len(nums)):
-            sol.append(path[:])
+            path.append(nums[i])
             dfs(start + 1, path + [nums[i]])
-            dfs(start+1,path)
+            dfs(start + 1, path)
+            sol.append(path[:])
             
 
     sol = []
