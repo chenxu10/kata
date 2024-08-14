dac36bbfa9fcf62d969563ca9bd3e3cb3eb29d96
chenxu10 <shenxu2011@gmail.com>
2024-08-14 15:27:18 -0400

    sds

---
+++ fundamentals/test/test_stronger_induction_for_tree/test_array_to_BST.py
@@ -0,0 +1,9 @@
+from fundamentals.manbercreativemethod.compute_balancing_factor_07 import sortedArrayToBST
+
+def test_build_height_balanced_tree():
+    Node = sortedArrayToBST([-3,0,5,9])
+    assert Node.left.val == -3
+    assert Node.right.val == 5
+    assert Node.right.right.val == 9
+
+
