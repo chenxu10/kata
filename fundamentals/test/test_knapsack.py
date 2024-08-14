dcb2f9886a8cf41caca6665e2206864ade0d3a9a
chenxu10 <shenxu2011@gmail.com>
2024-08-14 15:15:59 -0400

    working

---
+++ fundamentals/test/test_knapsack.py
@@ -0,0 +1,25 @@
+
+
+
+def knapsack(weights, capacity):
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for w in range(capacity + 1):
+            if weights[i-1] <= w:
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]] + weights[i-1])
+            else:
+                dp[i][w] = dp[i-1][w]
+    
+    return dp[n][capacity]
+
+
+
+
+def test_knapsack():
+    assert knapsack([3,4,10],9) == 7
+    assert knapsack([3,4,2],9) == 9
+
+
+test_knapsack()
