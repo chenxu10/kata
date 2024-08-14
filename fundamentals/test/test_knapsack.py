b13d51c6a6e2c0f4e342e97b8a6c46c6e0a90157
chenxu10 <shenxu2011@gmail.com>
2024-08-14 15:18:56 -0400

    working

--- fundamentals/test/test_knapsack.py
+++ fundamentals/test/test_knapsack.py
@@ -1,18 +1,7 @@
 
+from fundamentals.manbercreativemethod.knapsack_10 import knapsack
 
 
-def knapsack(weights, capacity):
-    n = len(weights)
-    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
-    
-    for i in range(1, n + 1):
-        for w in range(capacity + 1):
-            if weights[i-1] <= w:
-                dp[i][w] = max(dp[i-1][w], dp[i-1][w-weights[i-1]] + weights[i-1])
-            else:
-                dp[i][w] = dp[i-1][w]
-    
-    return dp[n][capacity]
 
 def test_knapsack():
     assert knapsack([3,4,10],9) == 7
