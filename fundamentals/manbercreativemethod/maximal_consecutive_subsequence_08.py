3b9bc5a82cac7554df91cf27e43c9934e47027ba
chenxu10 <shenxu2011@gmail.com>
2024-08-16 06:48:52 -0400

    working

--- fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
+++ fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
@@ -189,7 +189,8 @@ def maximum_sum_subsequence(nums,query):
     M = 10 ** 9 + 7
 
     for q in query:
-        res += root.update(q[0], q[1])
+        root.update(q[0], q[1])
+        res += max(root.info00, root.info01, root.info11, root.info10)
         res % M
 
     return res
