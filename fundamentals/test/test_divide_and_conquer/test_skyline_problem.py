08347f5e6bd5e7d22ad7040041ec5463d65c084e
chenxu10 <shenxu2011@gmail.com>
2024-08-16 10:27:44 -0400

    working

---
+++ fundamentals/test/test_divide_and_conquer/test_skyline_problem.py
@@ -0,0 +1,5 @@
+from fundamentals.manbercreativemethod import compute_balancing_factor_07 as cb
+
+def test_merge_sort():
+    assert cb.merge_sort[1] == [1]
+    assert cb.merge_sort[1,3,6,5] == [1,3,5,6]
