16a46151f3be18312323b6ebc8ecbc0de1ac5185
chenxu10 <shenxu2011@gmail.com>
2024-08-16 17:51:35 -0400

    working

--- fundamentals/test/test_divide_and_conquer/test_skyline_problem.py
+++ fundamentals/test/test_divide_and_conquer/test_skyline_problem.py
@@ -3,3 +3,6 @@ from fundamentals.manbercreativemethod import compute_balancing_factor_07 as cb
 def test_merge_sort():
     assert cb.merge_sort([1]) == [1]
     assert cb.merge_sort([1,3,6,5]) == [1,3,5,6]
+
+def test_skyline():
+    assert cb.getSkyline([[0,2,3],[2,5,3]]) == [5,0]
