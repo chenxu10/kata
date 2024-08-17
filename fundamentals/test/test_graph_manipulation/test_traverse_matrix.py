a15c9a6bcd526e0821d3ff43040529ef1cdd2d12
chenxu10 <shenxu2011@gmail.com>
2024-08-17 10:29:59 -0400

    working

--- fundamentals/test/test_graph_manipulation/test_traverse_matrix.py
+++ fundamentals/test/test_graph_manipulation/test_traverse_matrix.py
@@ -1,9 +1,42 @@
 
 def traverse_matrix(matrix, word):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    
+
+    def dfs(i,j,visited,cur_index):
+        # lead node
+        if cur_index == len(word):
+            return True
+        # terminate backtrack logic
+        if i < 0 or i >= rows or j < 0 or j >= cols:
+            return False
+        if visited[i][j]:
+            return False
+        if matrix[i][j] != word[cur_index]:
+            return False
+        
+        vectors = [(-1,0),(1,0),(0,1),(0,-1)]
+        visited[i][j] = 1
+        for v in vectors:
+            i += v[0]
+            j += v[1]
+            if dfs(i,j,visited,cur_index + 1):
+                return True
+        visited[i][j] = 0
+        return False
+
+    for i in range(rows):
+        for j in range(cols):
+            if dfs(i,j,visited,0):
+                return True
     return False
 
 def test_traverse_matrix():
-    assert traverse_matrix([[]],'abc') == False
+    #assert traverse_matrix([[]],'abc') == False
+    print(traverse_matrix([['a','b'],['c','d']],'abc'))
+    #assert traverse_matrix([['a','b'],['d','c']],'abc') == True
 
 def main():
     test_traverse_matrix()
