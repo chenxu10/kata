c1a8e3961232d0b49dae817fa53942825b12536c
chenxu10 <shenxu2011@gmail.com>
2024-08-17 08:53:51 -0400

    working

--- fundamentals/test/test_primefactor.py
+++ fundamentals/test/test_primefactor.py
@@ -3,13 +3,12 @@ def prime_factor(x):
     ans = []
     if x > 1:
         ans.append(x)
-        
+
     return ans
 def test_prime_factor():
     assert prime_factor(1) == []
     assert prime_factor(2) == [2]
-    # assert prime_factor(2) == [2]
-    # assert prime_factor(3) == [3]
+    assert prime_factor(3) == [3]
     # assert prime_factor(4) == [2,2]
     # assert prime_factor(5) == [5]
     # assert prime_factor(6) == [2,3]
