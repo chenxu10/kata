3be9e6ae7b8c19347c2fef0605b2bffebd02b428
chenxu10 <shenxu2011@gmail.com>
2024-08-17 04:47:24 -0400

    working

--- fundamentals/test/test_three_sum.py
+++ fundamentals/test/test_three_sum.py
@@ -6,12 +6,26 @@
 
 
 def three_sum(nums):
-    return []
+    ans = []
+
+    if len(nums) < 2:
+        return []
+    else:
+        sol = nums[0] + nums[1] + nums[2]
+        if sol == 0:
+            ans.append([nums[0],nums[1],nums[2]])
+    return ans
 
 
-def test_three_sum(x):
+def test_three_sum():
     assert three_sum([]) == []
     assert three_sum([1]) == []
     assert three_sum([0,1,1]) == []
     assert three_sum([-1,0,1]) == [[-1,0,1]]
+
+def main():
+    test_three_sum()
+    
+if __name__ == '__main__':
+    main()
 
