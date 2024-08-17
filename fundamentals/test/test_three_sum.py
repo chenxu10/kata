ece83e6ef42c7abb3703ba44d4234db1815384fb
chenxu10 <shenxu2011@gmail.com>
2024-08-17 04:57:46 -0400

    working

--- fundamentals/test/test_three_sum.py
+++ fundamentals/test/test_three_sum.py
@@ -20,6 +20,7 @@ def test_three_sum():
     assert three_sum([0,1,1]) == []
     assert three_sum([-1,0,1]) == [[-1,0,1]]
     assert three_sum([-1,0,3,1]) == [[-1,0,1]]
+    assert three_sum([-1,0,3,3,1]) == [[-1,0,1]]
 
 def main():
     test_three_sum()
