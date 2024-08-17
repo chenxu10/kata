d40da0154bd13d5c733ab127aae95f42f6ffba43
chenxu10 <shenxu2011@gmail.com>
2024-08-17 16:07:56 -0400

    sds

---
+++ fundamentals/test/test_graph_data_structure.py
@@ -0,0 +1,16 @@
+class G:
+    def __init__(self, v):
+        self.v = v
+        self.e = 0
+        self.adj_lists = [[]for i in range(v)]
+
+    def add_edge(self,v, w):
+        self.adj_lists[v].append(w)
+        self.adj_lists[w].append(v)
+        self.e += 1
+
+def test_adjacency_list_graph_structure():
+    graph = G(4)
+    graph.add_edge(1,2) #O(1)
+    assert graph.e == 1
+    assert (1 in i for i in graph.adj_lists)
