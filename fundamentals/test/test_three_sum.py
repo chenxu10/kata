3ee4132be5c80707576075dbc6272e58fa234015
chenxu10 <shenxu2011@gmail.com>
2024-08-17 04:40:34 -0400

    sds

---
+++ fundamentals/test/test_three_sum.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+def three_sum(nums):
+    return []
+
+
+def test_three_sum(x):
+    assert three_sum([]) == []
+    assert three_sum([1]) == []
+    assert three_sum([0,1,1]) == []
+
