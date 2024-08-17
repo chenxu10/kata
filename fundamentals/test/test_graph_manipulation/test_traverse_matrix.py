4aad1bed6512635240fe85d3df323d0ba848ba0f
chenxu10 <shenxu2011@gmail.com>
2024-08-17 09:13:51 -0400

    sds

---
+++ fundamentals/test/test_graph_manipulation/test_traverse_matrix.py
@@ -0,0 +1,12 @@
+
+def traverse_matrix(matrix, word):
+    return False
+
+def test_traverse_matrix():
+    assert traverse_matrix([[]],'abc') == False
+
+def main():
+    test_traverse_matrix()
+
+if __name__ == '__main__':
+    main()
