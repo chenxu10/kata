ee216ead3d16ba3a066d67581fd2f8963107ac39
chenxu10 <shenxu2011@gmail.com>
2024-08-17 10:42:47 -0400

    working

--- fundamentals/test/test_graph_manipulation/test_traverse_matrix.py
+++ fundamentals/test/test_graph_manipulation/test_traverse_matrix.py
@@ -6,24 +6,24 @@ def traverse_matrix(matrix, word):
     
 
     def dfs(i,j,cur_index):
-        nonlocal visited
         # lead node
         if cur_index == len(word):
             return True
         # terminate backtrack logic
         if i < 0 or i >= rows or j < 0 or j >= cols:
             return False
-        if visited[i][j] or matrix[i][j] != word[cur_index]:
+        if matrix[i][j] != word[cur_index]:
             return False
         
-        visited[i][j] = 1
+        temp = matrix[i][j]
+        matrix[i][j] = '#'
         vectors = [(-1,0),(1,0),(0,1),(0,-1)]
         for v in vectors:
             i += v[0]
             j += v[1]
             if dfs(i,j,cur_index + 1):
                 return True
-        visited[i][j] = 0
+        matrix[i][j] = temp
         return False
 
     for i in range(rows):
