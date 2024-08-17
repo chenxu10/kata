d2a32ca601a85b40a476e4837d78f58782fc6611
chenxu10 <shenxu2011@gmail.com>
2024-08-17 16:19:33 -0400

    working

--- fundamentals/test/test_graph_data_structure.py
+++ fundamentals/test/test_graph_data_structure.py
@@ -1,10 +1,20 @@
+"""
+Adjacent Lists of Graph
+"""
+
 class G:
     def __init__(self, v):
         self.v = v
         self.e = 0
         self.adj_lists = [[] for i in range(v)]
 
+    def _validate_vertex(self,v):
+        if v < 0 or v >= self.v:
+            raise ValueError(f"vertex {v} is not between 0 and vertices")
+
     def add_edge(self,v, w):
+        self._validate_vertex(v)
+        self._validate_vertex(w)
         self.adj_lists[v].append(w)
         self.adj_lists[w].append(v)
         self.e += 1
