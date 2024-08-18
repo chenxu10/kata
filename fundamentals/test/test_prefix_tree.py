2c35015d4ed61db2e338a52ba248ae2b1e7266f3
chenxu10 <shenxu2011@gmail.com>
2024-08-18 08:52:20 -0400

    sds

---
+++ fundamentals/test/test_prefix_tree.py
@@ -0,0 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+    
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+    
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+def test_prefix_tree():
+    trie = Trie()
+    trie.insert("apple")
+    #assert trie.search("app") == True
+    print(trie.starts_with("apple"))
+    assert trie.search("apples") == False
+
+def main():
+    test_prefix_tree()
+
+if __name__ == '__main__':
+    main()
+    # longest common subsequence(leetcode14)
