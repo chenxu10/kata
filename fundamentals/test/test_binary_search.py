bb704ce4969e497efd3da60208e4d83baf87fd32
chenxu10 <shenxu2011@gmail.com>
2024-08-19 06:55:38 -0400

    working

--- fundamentals/test/test_binary_search.py
+++ fundamentals/test/test_binary_search.py
@@ -2,17 +2,21 @@
 # mid, empty space return -1
 
 def binary_search(A,x):
+
     def recursive_search(low,high):
         if low > high:
             return -1
-        mid = (low + high) // 2        
+        mid = (low + high) // 2
         if A[mid] == x:
             return mid
-        if A[mid] > x:
-            return recursive_search(low, mid -1)
-        if A[mid] < x:
+        elif A[mid] > x:
+            return recursive_search(low, mid - 1)
+        else:
             return recursive_search(mid + 1, high)
+
+
     return recursive_search(0, len(A) - 1)
+
 
 def test_binary_search():   
     assert binary_search([1,1,2,2,34],34) == 4
