451a96a5bc2931e3e4bc48dbd2df23962d4df78f
chenxu10 <shenxu2011@gmail.com>
2024-08-19 06:31:48 -0400

    working

--- fundamentals/test/test_binary_search.py
+++ fundamentals/test/test_binary_search.py
@@ -1,23 +1,23 @@
 
 
-def binary_search(arr,start,end,k):
-    if arr == []:
-        return -1
-    arr.sort()
-    
-    if start > end:
-        return -1
-    mid = (start + end) // 2
-    if arr[mid] == k:
-        return mid
-    if arr[mid] > k:
-        return binary_search(arr, start, mid - 1, k)
-    else:
-        return binary_search(arr, mid + 1, end, k)
+def binary_search(A,x):
+    def recursive_search(low, high):
+        if low > high:
+            return -1  # x is not in the array
+        
+        mid = (low + high) // 2
+        
+        if A[mid] == x:
+            return mid  # x found at index mid
+        elif A[mid] > x:
+            return recursive_search(low, mid - 1)  # search in the left subarray
+        else:
+            return recursive_search(mid + 1, high)  # search in the right subarray
+
+    return recursive_search(0, len(A) - 1)
 
 def test_binary_search():   
-    assert binary_search([1,1,2,2,34],0,4,34) == 4
-    assert binary_search([],0,1,1) == -1
+    assert binary_search([1,1,2,2,34],34) == 4
 
 def main():
     test_binary_search()
