de6865630ebb3531d0156143263ca23ed158b928
chenxu10 <shenxu2011@gmail.com>
2024-08-19 06:24:53 -0400

    working

--- fundamentals/test/test_binary_search.py
+++ fundamentals/test/test_binary_search.py
@@ -3,19 +3,18 @@
 def binary_search(arr,start,end,k):
     arr.sort()
     
-    if len(arr) == 0:
-        return
+    if start > end:
+        return -1
     mid = (start + end) // 2
     if arr[mid] == k:
         return mid
     if arr[mid] > k:
-        binary_search(arr, start, mid - 1, k)
+        return binary_search(arr, start, mid - 1, k)
     else:
-        binary_search(arr, mid + 1, end, k)
+        return binary_search(arr, mid + 1, end, k)
 
-def test_binary_search():
-    assert binary_search([2,34,2,1,1],0,5,34) == 1
-
+def test_binary_search():   
+    print(binary_search([2,34,2,1,1],0,4,34))
 
 def main():
     test_binary_search()
