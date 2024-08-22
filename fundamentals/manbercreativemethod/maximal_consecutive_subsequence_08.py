ce4683c384dbfbd944d0f11b0c706ecc796f3ee0
chenxu10 <shenxu2011@gmail.com>
2024-08-22 14:11:25 -0400

    working

--- fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
+++ fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
@@ -201,7 +201,7 @@ def maximum_subarray(nums):
 
     for i in range(n):
         local_sum = nums[i]
-        print("localsum",local_sum,"expect -2")
+        print("localsum",local_sum)
         for j in range(i + 1, n):
             local_sum = local_sum + nums[j]
             print("endlocal sum",local_sum)
