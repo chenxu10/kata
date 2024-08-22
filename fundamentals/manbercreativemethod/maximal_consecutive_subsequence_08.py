b7e95b12a7fd9b7a7ba813ab7556b74f77f20acf
chenxu10 <shenxu2011@gmail.com>
2024-08-22 10:08:25 -0400

    working

--- fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
+++ fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
@@ -194,7 +194,7 @@ def maximum_subarray(nums):
     local_sum = nums[0]                        # sum from p to q
     local_max = float('-inf')                        # current local interval max              
     n = len(nums)
-    for i in range(n):
+    for i in range(n - 1):
         for j in range(i + 1, n):
             print("j",j)
             local_sum = local_sum + nums[j]
