c8ad1460de42fa70bfdd0def1df156aaca2416d4
chenxu10 <shenxu2011@gmail.com>
2024-08-23 11:58:23 -0400

    working

--- fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
+++ fundamentals/manbercreativemethod/maximal_consecutive_subsequence_08.py
@@ -234,6 +234,18 @@ def maximum_subarray_divide_conquer(nums):
     return max_subarray_recursive(nums, 0, len(nums) - 1)
 
 
+def maximum_subarray_dp(nums):
+    res = nums[0]
+    dp = [None for _ in range(len(nums))]
+    dp[0] = nums[0]
+
+    for i in range(1, len(nums)):
+        dp[i] = max(dp[i-1] + nums[i], nums[i])
+        res = max(res, dp[i])
+
+    return res
+
+
 
 
 
