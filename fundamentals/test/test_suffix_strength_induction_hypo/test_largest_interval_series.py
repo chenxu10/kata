e505a8206ff97cc5c3ef1f4a52a50e04121b4a45
chenxu10 <shenxu2011@gmail.com>
2024-08-23 09:25:13 -0400

    working

--- fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
+++ fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
@@ -1,4 +1,4 @@
-from fundamentals.manbercreativemethod.maximal_consecutive_subsequence_08 import maximum_subarray
+from fundamentals.manbercreativemethod.maximal_consecutive_subsequence_08 import maximum_subarray, maximum_subarray_divide_conquer
 
 def test_largest_interval_series():
     assert maximum_subarray([2,-1,5])==6
@@ -6,6 +6,9 @@ def test_largest_interval_series():
     assert maximum_subarray([5,4,-1,7,8]) == 23
     assert maximum_subarray([1]) == 1
     assert maximum_subarray([-1,-2]) == -1
+    assert maximum_subarray_divide_conquer([2,-1,5])==6
+    assert maximum_subarray_divide_conquer([2,5,-5,10,15,20,-5])==47
+
 def main():
     test_largest_interval_series()
 
