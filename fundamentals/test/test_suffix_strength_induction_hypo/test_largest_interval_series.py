79aae3120df584415f2c4020609678e9716deaab
chenxu10 <shenxu2011@gmail.com>
2024-08-23 13:59:16 -0400

    working

--- fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
+++ fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
@@ -1,13 +1,13 @@
 from fundamentals.manbercreativemethod.maximal_consecutive_subsequence_08 import maximum_subarray, maximum_subarray_divide_conquer
 
 def test_largest_interval_series():
-    # assert maximum_subarray([2,-1,5])==6
-    # assert maximum_subarray([2,5,-5,10,15,20,-5])==47
-    # assert maximum_subarray([5,4,-1,7,8]) == 23
-    # assert maximum_subarray([1]) == 1
+    assert maximum_subarray([2,-1,5])==6
+    assert maximum_subarray([2,5,-5,10,15,20,-5])==47
+    assert maximum_subarray([5,4,-1,7,8]) == 23
+    assert maximum_subarray([1]) == 1
     assert maximum_subarray([-1,-2]) == -1
-    #assert maximum_subarray_divide_conquer([2,-1,5])==6
-    #assert maximum_subarray_divide_conquer([2,5,-5,10,15,20,-5])==47
+    assert maximum_subarray_divide_conquer([2,-1,5])==6
+    assert maximum_subarray_divide_conquer([2,5,-5,10,15,20,-5])==47
     #print(maximum_subarray_divide_conquer([23.2,3.2,-1.4,-12.2,34.2,5.4]))
 
 def main():
