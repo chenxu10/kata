83fbf21a0df8632980c7d649454aba0a6de5c4f4
chenxu10 <shenxu2011@gmail.com>
2024-08-23 13:57:01 -0400

    working

--- fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
+++ fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
@@ -5,7 +5,7 @@ def test_largest_interval_series():
     # assert maximum_subarray([2,5,-5,10,15,20,-5])==47
     # assert maximum_subarray([5,4,-1,7,8]) == 23
     # assert maximum_subarray([1]) == 1
-    print(maximum_subarray([-1,-2])) == -1
+    assert maximum_subarray([-1,-2]) == -1
     #assert maximum_subarray_divide_conquer([2,-1,5])==6
     #assert maximum_subarray_divide_conquer([2,5,-5,10,15,20,-5])==47
     #print(maximum_subarray_divide_conquer([23.2,3.2,-1.4,-12.2,34.2,5.4]))
