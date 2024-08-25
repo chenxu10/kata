5a6886d3b46a0f001928a10417aec4c5ea7bef80
chenxu10 <shenxu2011@gmail.com>
2024-08-24 21:52:02 -0400

    sds

---
+++ fundamentals/test/test_divide_and_conquer/test_merge_sort.py
@@ -0,0 +1,9 @@
+from fundamentals.manbercreativemethod.compute_balancing_factor_07 import merge_sort_memory_improve
+
+def test_merge_sort(arr):
+    """
+    """
+    arr = [2,3,4,1,-1]
+    assert(merge_sort_memory_improve(arr,0,len(arr)-1)==[-1,1,2,3,4])
+
+
