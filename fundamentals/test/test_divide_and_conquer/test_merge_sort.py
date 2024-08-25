5571838a5a9b1a86971de52638588f654715f075
chenxu10 <shenxu2011@gmail.com>
2024-08-25 08:24:40 -0400

    working

--- fundamentals/test/test_divide_and_conquer/test_merge_sort.py
+++ fundamentals/test/test_divide_and_conquer/test_merge_sort.py
@@ -3,7 +3,7 @@ from fundamentals.manbercreativemethod.compute_balancing_factor_07 import merge_
 def test_merge_sort():
     """
     """
-    arr = [2,3,4,1,-1]
+    arr = [1,3,2,4]
     merge_sort_memory_improve(arr, 0, len(arr) - 1)
     # assert(merge_sort_memory_improve(
     #     arr,0,len(arr)-1)==[-1,1,2,3,4])
