a91e765d0370454787a6746765a7f0ab2107e68a
chenxu10 <shenxu2011@gmail.com>
2024-08-29 16:56:27 -0400

    working

--- fundamentals/test/test_word_searchII.py
+++ fundamentals/test/test_word_searchII.py
@@ -72,7 +72,8 @@ def test_word_search_two():
 def main():
     #test_word_search_two()
     curnode = build_each_word_node('aac', TrieNode())
-    print(curnode.nodes)#print(root.nodes[0].nodes[0].nodes[1].word)
+    print(curnode.nodes)
+    nodes = build_trie_on_words(['aac'])#print(root.nodes[0].nodes[0].nodes[1].word)
 
 if __name__ == '__main__':
     main()
