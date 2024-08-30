e5107c102dbd1717b488aefecd91707cd66b8590
chenxu10 <shenxu2011@gmail.com>
2024-08-30 12:08:05 -0400

    working

--- fundamentals/test/test_three_sum.py
+++ fundamentals/test/test_three_sum.py
@@ -1,36 +1,35 @@
 
 def three_sum(nums):
-    ans = []
-
-    if len(nums) < 2:
-        return []
-    
     nums.sort()
     n = len(nums)
-    for i in range(len(nums)):
-        j = i + 1
-        k = n - 1
+    res = []
+    
+    for i in range(n):
+        if i == 0 or (nums[i - 1] != nums[i]):
+            j = i + 1
+            k = n - 1
         while j < k:
-            sol = nums[i] + nums[j] + nums[k]
-            if sol == 0:
-                ans.append([nums[i],nums[j],nums[k]])
+            ans = nums[i] + nums[j] + nums[k]
+            if ans == 0:
+                res.append([nums[i], nums[j], nums[k]])
                 j += 1
-            if sol > 0:
+                while j < k and nums[j - 1] == nums[j]:
+                    j += 1
+            if ans > 0:
                 k -= 1
-            if sol < 0:
+            if ans < 0:
                 j += 1
-    return ans
 
+    return res
 
 def test_three_sum():
     assert three_sum([]) == []
     assert three_sum([1]) == []
     assert three_sum([0,1,1]) == []
     assert three_sum([-1,0,1]) == [[-1,0,1]]
-    print(three_sum([-1,0,3,1]))
     assert three_sum([-1,0,3,1]) == [[-1,0,1]]
     assert three_sum([-1,0,3,3,1]) == [[-1,0,1]]
-
+    assert three_sum([-1,0,1,2,-1,-4]) == [[-1,-1,2],[-1,0,1]]
 def main():
     test_three_sum()
     
