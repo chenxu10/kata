2ec16034956231c797cf33ce652847c8e9711d37
chenxu10 <shenxu2011@gmail.com>
2024-08-30 11:09:52 -0400

    working

--- fundamentals/test/test_dp_knapsack/test_2d_map.py
+++ fundamentals/test/test_dp_knapsack/test_2d_map.py
@@ -1,7 +1,13 @@
 from fundamentals.mianjing.fb_lc_62_20240902 import unique_paths
+from fundamentals.mianjing.fb_lc_62_20240902 import unique_paths_graph
 
 def test_unique_paths():
     assert unique_paths(3, 7) == 28
     assert unique_paths(3, 2) == 3
 
+def test_unique_paths_graph():
+    assert unique_paths_graph(3, 7) == 28
+    assert unique_paths_graph(3, 2) == 3
+
 test_unique_paths()
+test_unique_paths_graph()
