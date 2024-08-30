350fada258cf8b7757b2bc11b7d0abd5ed7a1d16
chenxu10 <shenxu2011@gmail.com>
2024-08-30 11:36:03 -0400

    sds

---
+++ fundamentals/mianjing/fb_duplicate_characters_20240830.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def max_length_duplicate_contiguous(s):
+    counter = Counter(s)
+    max_length = max(counter.values())
+    result = [char for char, count in counter.items() if count == max_length]
+    return result   
+
+
+
+assert max_length_duplicate_contiguous('aaaabbbbccc') == ['a','b']  # Should return ['a', 'b']
+assert max_length_duplicate_contiguous('abcd') == ['a','b','c','d']  # Should return ['a', 'b', 'c', 'd']
