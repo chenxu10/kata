3b63b457c6df9c7f3d97c05ea307f175ddcbd9df
chenxu10 <shenxu2011@gmail.com>
2024-08-30 11:10:13 -0400

    working

--- fundamentals/test/test_data_structure/test_graph_data_structure.py
+++ fundamentals/test/test_data_structure/test_graph_data_structure.py
@@ -26,38 +26,3 @@ def test_adjacency_list_graph_structure():
     graph.add_edge(1,2) #O(1)
     assert graph.e == 1
     assert (1 in i for i in graph.adj_lists)
-
-class Graph:
-    def __init__(self, rows, cols):
-        self.rows = rows
-        self.cols = cols
-        self.adj_list = {}
-        self._build_graph()
-
-    def _build_graph(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                node = (i, j)
-                self.adj_list[node] = []
-                if i < self.rows - 1:
-                    self.adj_list[node].append((i+1, j))
-                if j < self.cols - 1:
-                    self.adj_list[node].append((i, j+1))
-
-    def dfs(self, start, end, path, paths):
-        if start == end:
-            paths.append(path)
-            return
-        for neighbor in self.adj_list[start]:
-            self.dfs(neighbor, end, path + [neighbor], paths)
-
-def unique_paths_graph(m, n):
-    graph = Graph(m, n)
-    start = (0, 0)
-    end = (m-1, n-1)
-    paths = []
-    graph.dfs(start, end, [start], paths)
-    return len(paths)
-
-# Test
-print(unique_paths_graph(3, 7))  # Should print 28
