82f1b0afec14511305be0085ac6db13d37da0911
chenxu10 <shenxu2011@gmail.com>
2024-08-30 11:09:50 -0400

    working

--- fundamentals/mianjing/fb_lc_62_20240902.py
+++ fundamentals/mianjing/fb_lc_62_20240902.py
@@ -16,4 +16,34 @@ def unique_paths_space_optimized(m, n):
             dp[j] += dp[j-1]
     return dp[-1]
 
+class Graph:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.adj_list = {}
+        self._build_graph()
 
+    def _build_graph(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                node = (i, j)
+                self.adj_list[node] = []
+                if i < self.rows - 1:
+                    self.adj_list[node].append((i+1, j))
+                if j < self.cols - 1:
+                    self.adj_list[node].append((i, j+1))
+
+    def dfs(self, start, end, path, paths):
+        if start == end:
+            paths.append(path)
+            return
+        for neighbor in self.adj_list[start]:
+            self.dfs(neighbor, end, path + [neighbor], paths)
+
+def unique_paths_graph(m, n):
+    graph = Graph(m, n)
+    start = (0, 0)
+    end = (m-1, n-1)
+    paths = []
+    graph.dfs(start, end, [start], paths)
+    return len(paths)
