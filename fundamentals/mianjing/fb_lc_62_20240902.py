e5b7d4b6dae1f23ddcad1c72fbea6e33dd8ac84c
chenxu10 <shenxu2011@gmail.com>
2024-08-29 20:32:51 -0400

    working

--- fundamentals/mianjing/fb_lc_62_20240902.py
+++ fundamentals/mianjing/fb_lc_62_20240902.py
@@ -1,6 +1,19 @@
 def unique_paths(m, n):
+    dp = [[0 for _ in range(n)] for _ in range(m)]
+    for i in range(m):
+        dp[i][0] = 1
+    for j in range(n):
+        dp[0][j] = 1
+    for i in range(1,m):
+        for j in range(1,n):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[-1][-1]
+
+def unique_paths_space_optimized(m, n):
     dp = [1] * n
     for i in range(1,m):
         for j in range(1,n):
             dp[j] += dp[j-1]
     return dp[-1]
+
+
