24ee64b4637c6575527ca7a8b43be550dff31064
chenxu10 <shenxu2011@gmail.com>
2024-08-31 17:50:04 -0400

    sds

---
+++ fundamentals/test/test_data_structure/test_trie_build.py
@@ -0,0 +1,25 @@
+class Trie:
+    def __init__(self):
+        self.nodes = [None] * 26
+        self.word = None
+    
+def build_word(word, root):
+    "root is mutuable"
+    cur = root
+    for c in word:
+        idx = ord(c) - ord('a')
+        if not cur.nodes[idx]:
+            cur.nodes[idx] = Trie()
+        cur = cur.nodes[idx]
+    cur.word = word
+    return root
+
+def test_trie():
+    t = Trie()
+    cur = build_word('abc',t)
+    print(cur.nodes)
+
+def main():
+    test_trie()
+if __name__ == '__main__':
+    main()
