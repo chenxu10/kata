778df3e347f58a2fb99170c1646c2fe82eb67f80
chenxu10 <shenxu2011@gmail.com>
2024-08-31 20:11:37 -0400

    working

--- fundamentals/test/test_data_structure/test_suffix_tree.py
+++ fundamentals/test/test_data_structure/test_suffix_tree.py
@@ -1,50 +1,39 @@
-
-class SuffixTreeNode:
-    def __init__(self):
+class SuffixNode():
+    def __init__(self) -> None:
         self.children = {}
         self.is_end = False
 
-class SuffixTree:
-    def __init__(self, s):
+class SuffixTree():
+    def __init__(self, s) -> None:
         self.s = s
         self.root = self._build_tree()
 
     def _build_tree(self):
-        root = SuffixTreeNode()
+        root = SuffixNode()
+        
         for i in range(len(self.s)):
-            self._insert_suffix(self.s[i:],root)
+            self._insert_suffix(self.s[i:], root)
+
         return root
     
-    def _insert_suffix(self, suffix, root):
-        cur = root
-        for c in suffix:
-            if c not in cur.children:
-                cur.children[c] = SuffixTreeNode()
-            cur = cur.children[c]
-        cur.is_end = True
-        
+    def _insert_suffix(self, word, node):
+        for c in word:
+            if c not in node.children:
+                node.children = SuffixNode()
+            node.childeren = node.children[c]
+        node.is_end = True
+        return node
     
-    def search(self, suffix):
-        cur = self.root
-        for c in suffix:
-            if c not in cur.children:
+    def search(self, pattern):
+        for c in pattern:
+            if c not in self.root.children:
                 return False
-            cur = cur.children[c]            
         return True
 
 def test_suffix_tree():
-    tree = SuffixTree("banana$")
-    root = tree._build_tree()
-    assert root.children['b'].is_end==False
-    assert tree.search("ana") == True
-    assert tree.search("cdf") == False
-    assert tree.search("banana$") == True
-    assert tree.search("Xana") == False
-    # assert tree.search("na$") == True
-    # assert tree.search("") == True
-
-def main():
-    test_suffix_tree()
-
-if __name__ == "__main__":
-    main()
+    s = "bananas"
+    root = SuffixTree(s)
+    assert root["b"].is_end == False
+    assert isinstance(root["b"].nodes, dict)
+    assert root.search("ananas") == True
+    assert root.search("nas") == False
