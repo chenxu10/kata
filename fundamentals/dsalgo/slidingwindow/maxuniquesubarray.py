21d344e622925998bb57df95870779233a3b44d4
chenxu10 <shenxu2011@gmail.com>
2024-09-02 11:53:39 -0400

    sds

---
+++ fundamentals/dsalgo/slidingwindow/maxuniquesubarray.py
@@ -0,0 +1,17 @@
+def maxUniqueSubarraySum(nums):
+    left = 0 
+    max_sum = 0
+    cur_sum = 0
+    unique_set = set()
+
+    for right in range(len(nums)):
+        while nums[right] in unique_set:
+            unique_set.remove(nums[left])
+            cur_sum -= nums[left]
+            left += 1
+        
+        unique_set.add(nums[right])
+        cur_sum += nums[right]
+        max_sum = max(max_sum, cur_sum)
+
+    return max_sum
