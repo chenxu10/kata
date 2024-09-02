263e0935f8377583f16bc22a35924f246500df97
chenxu10 <shenxu2011@gmail.com>
2024-09-02 11:48:58 -0400

    sds

---
+++ fundamentals/test/test_pointer_manipulation/test_hashslidingwindow.py
@@ -0,0 +1,23 @@
+
+
+def maxUniqueSubarraySum(nums):
+    left = 0 
+    max_sum = 0
+    cur_sum = 0
+    unique_set = set()
+
+    for right in range(len(nums)):
+
+        while nums[right] in unique_set:
+            unique_set.remove(nums[left])
+            cur_sum -= nums[left]
+            left += 1
+        
+        unique_set.add(nums[right])
+        cur_sum += nums[right]
+        max_sum = max(cur_sum, cur_sum)
+
+    return max_sum
+
+nums = [5, 3, 5, 1, 4, 8, 9]
+assert maxUniqueSubarraySum(nums) == 30
