1778d3d8cc584a12ada1db65ecedfc0dedf1402e
chenxu10 <shenxu2011@gmail.com>
2024-09-07 19:36:39 -0400

    working

--- fundamentals/test/test_learning_insert_interval.py
+++ fundamentals/test/test_learning_insert_interval.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+import heapq
 
 def sweep_line_algorithm(events: List[Tuple[int, int]]) -> List[List[int]]:
     OPEN, CLOSE = 0, 1
@@ -56,9 +57,27 @@ def insert(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]
 
     return sweep_line_algorithm(events)
 
+
+def insert_heap(intervals, newInterval):
+    # Convert intervals to a list of tuples for easier heap operations
+    heap = [(i[0], i[1], i) for i in intervals + [newInterval]]
+    heapq.heapify(heap)
+    
+    result = []
+    while heap:
+        start, end, interval = heapq.heappop(heap)
+        
+        if not result or start > result[-1][1]:
+            result.append(interval)
+        else:
+            result[-1][1] = max(result[-1][1], end)
+    
+    return result
+
 # Test case
 def test_sweep_line():
     assert insert([[1,3],[6,9]], [2,5]) == [[1,5],[6,9]]
+    assert insert_heap([[1,3],[6,9]],[2,5]) == [[1,5],[6,9]]
     print("Test case passed!")
 
 if __name__ == "__main__":
