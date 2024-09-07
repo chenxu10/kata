b6632490a00b5dea83c0fee42b4e4ae2da9667f2
chenxu10 <shenxu2011@gmail.com>
2024-09-07 07:37:30 -0400

    working

--- fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
+++ fundamentals/test/test_suffix_strength_induction_hypo/test_largest_interval_series.py
@@ -10,27 +10,10 @@ def test_largest_interval_series():
     assert maximum_subarray_divide_conquer([2,5,-5,10,15,20,-5])==47
     #print(maximum_subarray_divide_conquer([23.2,3.2,-1.4,-12.2,34.2,5.4]))
 
-def calculate_cross_sum(nums, low, mid, high):
-    left_sum = float('-inf')
-    local_sum = 0
-
-    for i in range(mid, low - 1, -1):
-        local_sum += nums[i]
-        left_sum = max(left_sum, local_sum)
-
-    right_sum = float('-inf')
-    local_sum = 0
-
-    for j in range(mid + 1, high + 1):
-        local_sum += nums[j]
-        right_sum = max(right_sum, local_sum)
-
-    return left_sum + right_sum
-
 
 
 def test_calculate_cross_sum():
-    assert calculate_cross_sum([-1,2,3,-1]) == 5
+    assert calculate_cross_sum([-1,2,3,-1],0,5) == 5
 
 def main():
     test_largest_interval_series()
