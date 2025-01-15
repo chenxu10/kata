96715c52c0b5098bb83abaa776c7ecf838f3d808
chenxu10 <shenxu2011@gmail.com>
2025-01-15 07:38:50 -0500

    working

--- fundamentals/test/test_money_example.py
+++ fundamentals/test/test_money_example.py
@@ -1,8 +1,13 @@
 # TODO:five.times(2)
 # TODO: 5 dollor * 10 chf = 20 dollars if rate is 2:1
 
-
+class Dollar:
+    def __init__(self, amount) -> None:
+        self.amount = amount 
+    
+    def times(self, multipler):
+        self.amount = self.amount * multipler
 
 def test_multiply():
-    five = Money()
-    assert 10 == five.times(2)
+    five = Dollar(5)
+    assert 10 == five.times(2).amount
