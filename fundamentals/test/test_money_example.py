4fe46a5e9c4be3b4adcc9fa5b9ab55db82782966
chenxu10 <shenxu2011@gmail.com>
2025-01-22 05:54:27 -0500

    working

--- fundamentals/test/test_money_example.py
+++ fundamentals/test/test_money_example.py
@@ -6,15 +6,15 @@ class Dollar:
     def __init__(self,amount):
         self.amount = amount
 
-    def times(self, multipler):
-        self.amount *= multipler 
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
 
 def test_money_times():
     five = Dollar(5)
-    five.times(2)
-    assert five.amount == 10
-
-
+    dollar = five.times(2)
+    assert dollar.amount == 10
+    dollar = five.times(3)
+    assert dollar.amount==15
 
 def main():
     test_money_times()
