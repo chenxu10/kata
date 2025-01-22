c29cf7d87db1c25055f6e649c55871d15b55d056
chenxu10 <shenxu2011@gmail.com>
2025-01-22 05:28:55 -0500

    working

--- fundamentals/test/test_money_example.py
+++ fundamentals/test/test_money_example.py
@@ -2,7 +2,13 @@
 
 
 
+class Dollar:
+    def __init__(self,value):
+        self.value = value
 
+    def times(self, amount):
+        self.value *= amount
+        return self.value
 
 def test_money_times():
     five = Dollar(5)
