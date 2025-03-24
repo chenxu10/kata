cda1471e3cdd466c5cb740e5fddcee86532bff1b
chenxu10 <shenxu2011@gmail.com>
2025-03-23 21:36:42 -0400

    working

--- fundamentals/test/test_money_example.py
+++ fundamentals/test/test_money_example.py
@@ -1,23 +1,4 @@
 
 
-#TODO:edit in vim way DONOT leave the home row mouse is slow
 
-class Dollar:
-    def __init__(self,amount):
-        self.amount = amount
 
-    def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
-
-def test_money_times():
-    five = Dollar(5) 
-    dollar = five.times(2)
-    assert dollar.amount == 10
-    dollar = five.times(3)
-    assert dollar.amount==15
-
-def main():
-    test_money_times()
-
-if __name__ == '__main__':
-    main()
