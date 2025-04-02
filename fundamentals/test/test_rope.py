0a9e028bb80f83023d5e4ea654e393dea32715c2
chenxu10 <shenxu2011@gmail.com>
2025-04-02 08:51:27 -0400

    working

--- fundamentals/test/test_rope.py
+++ fundamentals/test/test_rope.py
@@ -1,5 +1,13 @@
 
 def to_rope(s):
-    return s
+    return Rope(s)
+
+
+class Rope:
+    def __init__(self, s):
+        self.s = s
+
+    def __str__(self) -> str:
+        return self.s
 
 assert str(to_rope("abc")) == 'abc'
