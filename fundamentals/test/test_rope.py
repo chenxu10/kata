fdfc05dd43b8351727cf5277de84baea62b6d86d
chenxu10 <shenxu2011@gmail.com>
2025-04-08 07:55:33 -0400

    working

--- fundamentals/test/test_rope.py
+++ fundamentals/test/test_rope.py
@@ -1,28 +1,15 @@
-import pandas as pd
-
-def to_rope(s):
-    return Rope(s)
-
-
-class Rope:
-    def __init__(self, s):
-        self.s = s
-
-    def __str__(self) -> str:
-        return self.s
-
-assert str(to_rope("abc")) == 'abc'
 
 
 
-def generate_tweedie_dataset():
-    return pd.DataFrame({
-      "net_loss":[1000,1000],  
-    })
 
-print(generate_tweedie_dataset())
 
-pd.testing.assert_frame_equal(
-    generate_tweedie_dataset(),
-    pd.DataFrame({"net_loss":[1000,1000]})
-)
+
+
+
+
+
+def test_rope():
+    pass
+
+
+
