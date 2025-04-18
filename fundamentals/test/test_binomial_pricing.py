29b8398d4861b123fa57dfa6e83d437bd50462af
chenxu10 <shenxu2011@gmail.com>
2025-04-18 10:27:13 -0400

    working

--- fundamentals/test/test_binomial_pricing.py
+++ fundamentals/test/test_binomial_pricing.py
@@ -1,12 +1,28 @@
+import numpy as np
 
-def calcuate_risk_netural_probability(T,N):
+def calcuate_risk_netural_probability(T,N,r,u,d):
     dt = T/N
-    return dt
+    risk_netual_prob = (np.exp(r * dt) - d)/(u - d)
+    return risk_netual_prob
+
+def intialize_stock_price_with_tree_structure(N, S0, u, d):
+    stock_tree = np.zeros((N+1, N+1))
+    for i in range(N+1):
+        for j in range(i+1):
+            stock_tree[j, i] = S0 * (u ** (i-j)) * (d ** j)
+    return stock_tree
 
 def binomial_pricing(params):
+    S0 = params['initial_price']
     T = params['time_to_expire_in_years']
     N = params['n_period']
-    prob = calcuate_risk_netural_probability(T,N)
+    r = params['annual_interest_rate']
+    u = params['up_factor']
+    d = params['down_factor']
+
+    risk_netural_prob = calcuate_risk_netural_probability(T,N,r,u,d)
+    stock_tree = intialize_stock_price_with_tree_structure(N, S0, u, d)
+
     return 2.01
 
 def test_binomial_pricing():
