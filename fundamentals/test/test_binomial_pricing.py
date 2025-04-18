8d3fdf07c033625448954864ee2a4c34a192df55
chenxu10 <shenxu2011@gmail.com>
2025-04-18 10:03:30 -0400

    working

--- fundamentals/test/test_binomial_pricing.py
+++ fundamentals/test/test_binomial_pricing.py
@@ -7,7 +7,14 @@ def binomial_pricing(params):
     return 2.01
 
 def test_binomial_pricing():
-    params = {}
+    params = {
+        'initial_price':100,
+        'strike_price':100,
+        'annual_interest_rate':0.04,
+        'n_period':3,
+        'up_factor':1.1,
+        'down_factor':1/1.1
+    }
     price = binomial_pricing(params)
     assert price == 2.01
 
